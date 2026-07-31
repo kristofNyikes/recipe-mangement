804d3a6c6a8752eecd59f2df8ef9b33d9a56d34d
Kristóf Nyikes <kristof.nyikes@gmail.com>
2026-07-31 09:51:45 +0200

    feat: not found page redirectsto /main if on main page dev only

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,13 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NotFound = () => {
+  const pathName = usePathname();
+
+  const isMainRoute = pathName?.startsWith("/main");
+
+  const homePath = isMainRoute ? "/main" : "/";
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
       <div className="flex items-center gap-3">
@@ -13,7 +20,7 @@ const NotFound = () => {
         The page you're looking for doesn't exist or has been moved.
       </p>
 
-      <Link href="/" className="btn btn-primary mt-8">
+      <Link href={homePath} className="btn btn-primary mt-8">
         ← Back to Home
       </Link>
     </div>
