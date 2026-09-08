7dd7bcef14a7a3c387b7c63ba52f64f65793fb2a
Kristóf Nyikes <kristof.nyikes@gmail.com>
2026-09-08 10:31:06 +0200

    feat: create get all favorite recipes endpoint

---
+++ app/api/v1/recipe/favorite/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const recipes = await prisma.recipe.findMany({
+      where: { isFavorite: true },
+    });
+
+    return NextResponse.json(
+      { data: recipes, count: recipes.length },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Failed to get favorite recipes:", error);
+
+    return NextResponse.json(
+      { error: "Failed to get favorite recipes" },
+      { status: 500 },
+    );
+  }
+};
