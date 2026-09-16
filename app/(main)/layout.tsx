77fe850b43a5608229435f228f6f073e3bcef823
Kristóf Nyikes <kristof.nyikes@gmail.com>
2026-09-16 13:36:26 +0200

    feat: add nextjs-toploader for navigation feedback

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar/AfterAuthNavbar";
+import NextTopLoader from "nextjs-toploader";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <NextTopLoader height={5} color="#9fe88d" />
       <main className="flex-1">{children}</main>
     </div>
   );
--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,9 +1,11 @@
+import NextTopLoader from "nextjs-toploader";
 import Navbar from "../Components/Navbar/BeforeAuthNavbar";
 
 const PublicLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <NextTopLoader height={5} color="#9fe88d" />
       <main className="flex-1">{children}</main>
     </div>
   );
