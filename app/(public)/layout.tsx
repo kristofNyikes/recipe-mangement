bf2131476278df3aa148af1e481754ceb33975db
Kristóf Nyikes <kristof.nyikes@gmail.com>
2026-09-18 08:49:45 +0200

    fix: remove spinner from toploader

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -5,7 +5,7 @@ const PublicLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <NextTopLoader height={5} color="#9fe88d" />
+      <NextTopLoader height={5} color="#9fe88d" showSpinner={false} />
       <main className="flex-1">{children}</main>
     </div>
   );
